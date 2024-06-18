d36ce880809445767d4c3e4b0fe660ad77495408
Christoph Langer <og.clanger@gmail.com>
2024-06-18 18:18:32 +0200

    feat(ui): restructure and implement button comp

--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -1,9 +1,26 @@
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 // We want each package to be responsible for its own content.
 const config: Omit<Config, "content"> = {
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        lg: "1024px",
+      },
+    },
     extend: {
+      colors: {
+        foreground: colors.zinc[900],
+        background: colors.white,
+        border: colors.zinc[200],
+        primary: colors.zinc,
+        accent: colors.orange,
+        "card-bg": colors.zinc[50],
+        ring: colors.orange[500],
+      },
       backgroundImage: {
         "glow-conic":
           "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
---
+++ packages/ui/src/components/button.tsx
@@ -0,0 +1,47 @@
+import { Slot } from "@radix-ui/react-slot";
+import { VariantProps, cva } from "class-variance-authority";
+import React from "react";
+import { cn } from "../utils";
+
+const buttonVariants = cva(
+  "font-medium text-white rounded-md transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none focus-visible:ring-offset-background focus-visible:ring-ring",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-accent-500 border border-accent-400 shadow-inner shadow-white/20",
+        outline: "border border-border hover:bg-zinc-100",
+      },
+      size: { default: "text-sm px-2.5 py-1" },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  },
+);
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    const { asChild = false, variant, size, className, ...rest } = props;
+
+    const Component = asChild ? Slot : "button";
+
+    return (
+      <Component
+        ref={ref}
+        {...rest}
+        className={cn(buttonVariants({ variant, size, className }))}
+      />
+    );
+  },
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
